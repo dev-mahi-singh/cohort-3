1fbd62a8fe4456e6d11aa53b2efa4e7bfb8f87a8
Mahi Singh <singh.dev.mahi@gmail.com>
2026-07-03 22:47:30 +0530

    primitive data type

--- javascript/phase1-foundation/script.js
+++ javascript/phase1-foundation/script.js
@@ -3,7 +3,7 @@
 // console.error("This is Error");
 // console.table(['harsh', 'adarsh', 'sarthak']);
 
-// variables 
+// variables
 
 // var a; // declaration
 // a = 10; // initialization
@@ -21,3 +21,39 @@
 // var a = 10; // number
 // var b = "Mahii" // String
 // var c = true; // boolean
+
+// Data Types
+//  ->Primitive
+//      - Number (10, 20, 22.4)
+//      - String ('h', "Sheryians Coding School")
+//      - Boolean (true, false)
+//      - undefined (variable is declared but not initialized then default value assigned by JS engine is undefined)
+//      - null (nothing, can be used to intentionally make variable empty)
+//      - BigInt (number's range is 2^53-1 and beyond that is BigInt)
+//      - Symbol (unique value)
+//  ->Non-Primitive (Reference)
+//      - Array
+//      - Object
+//      - Function
+
+// undefined - a variable has been declared but not yet assigned a value.
+// var a;
+// console.log(a);
+
+// null
+// var a = null;
+// console.log(a);
+
+// BigInt
+// var a = 10n;
+// console.log(a);
+
+// Symbol
+var s1 = Symbol('hi');
+var s2 = Symbol('hi');
+console.log(s1);
+console.log(s2);
+console.log(s1 === s2);
+
+
+

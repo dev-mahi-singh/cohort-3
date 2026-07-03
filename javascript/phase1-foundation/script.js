feea130dc3920aab52837d0545a3fabd421bd6e6
Mahi Singh <singh.dev.mahi@gmail.com>
2026-07-03 20:21:36 +0530

    variables in js

---
+++ javascript/phase1-foundation/script.js
@@ -0,0 +1,23 @@
+// console.log('hello js');
+// console.warn("This is warning");
+// console.error("This is Error");
+// console.table(['harsh', 'adarsh', 'sarthak']);
+
+// variables 
+
+// var a; // declaration
+// a = 10; // initialization
+
+// var a = 20;
+// var b = 5;
+// console.log(a, b);
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+
+// DataTypes
+// var a = 10; // number
+// var b = "Mahii" // String
+// var c = true; // boolean

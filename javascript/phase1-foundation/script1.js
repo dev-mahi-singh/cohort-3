989857badaa0f73b977bfab3762833e3a50f6e45
Mahi Singh <singh.dev.mahi@gmail.com>
2026-07-04 14:59:35 +0530

    operators in js

--- javascript/phase1-foundation/script1.js
+++ javascript/phase1-foundation/script1.js
@@ -3,14 +3,98 @@
 //  - Implicit
 //  - Explicit
 
-var a = '10';
-var b = '20';
-console.log(a + b);
-var a2 = Number(a);
-console.log(typeof (a2));
+// var a = '10';
+// var b = '20';
+// console.log(a + b);
+// var a2 = Number(a);
+// console.log(typeof (a2));
 
+// var a = Number(prompt("Enter number 1"));
+// var b = Number(prompt("Enter number 2"));
+// console.log(a + b);
 
+// Binary Operators
+//     - Arithmetic Operators (+, -, *, /, %)
+//     - Assignment Operators
+//     - Comparision Operators
+//     - Increment and Decrement Operators
+//     - Logical Operators (&&, ||)
 
+// Arithmetic Operators
+// var a = 30;
+// var b = 10;
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
 
+// Increment
+// var a = 10;
+// a++;
+// a++;
+// console.log(a);
 
+// post increment
+// var a = 10;
+// console.log(a++);
+// console.log(a);
 
+// pre increment
+// var a = 10;
+// console.log(++a);
+// console.log(a);
+
+// Decrement
+// var a = 10;
+// a--;
+// a--;
+// console.log(a);
+
+// pre decrement
+// var a = 10;
+// console.log(--a);
+// console.log(a);
+
+// post decrement
+// var a = 10;
+// console.log(a--);
+// console.log(a);
+
+// Assignment Operator
+// var a = 10;
+// a += 5;
+// a -= 4;
+// a *= 2;
+// console.log(a);
+
+// Comparision Operator
+// var a = 10;
+// var b = 20;
+// console.log(a == b);
+// console.log(a < b);
+// console.log(a > b);
+// console.log(a <= b);
+// console.log(a >= b);
+
+// var a = 10;
+// var b = '10';
+// console.log(a == b);
+// console.log(a === b);
+// console.log(a != b);
+// console.log(a !== b);
+
+// Logical Operator
+var a = 10;
+var b = 20;
+var c = 30;
+var d = 40;
+console.log(a > b || c < d);
+console.log(a < b || c > d);
+console.log(a > b || c > d);
+console.log(a < b || c < d);
+
+console.log(a > b && c < d);
+console.log(a < b && c > d);
+console.log(a > b && c > d);
+console.log(a < b && c < d);

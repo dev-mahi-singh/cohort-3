95df6442a66f978a2ea5e211b50e41e29a104c29
Mahi Singh <singh.dev.mahi@gmail.com>
2026-07-05 10:32:36 +0530

    while loop

---
+++ javascript/phase1-foundation/loops/script.js
@@ -0,0 +1,69 @@
+// Loops
+
+// while loop
+
+// var a = 1;
+// console.log("Before loop");
+// while (a < 5) {
+//     console.log(a);
+//     a++;
+// }
+// console.log('After loop');
+
+// Print number 1 to 10
+/*
+var a = 1;
+while (a <= 10) {
+    console.log(a);
+    a++;
+}
+*/
+
+// Print number 10 to 1
+/*
+var a = 10;
+while (a > 0) {
+    console.log(a);
+    a--;
+}
+*/
+
+// var num = Number(prompt("Enter a number"));
+// var a = 0;
+// while (a <= num) {
+//     console.log(a);
+//     a++;
+// }
+
+// Print Even Numbers
+/*
+var num = Number(prompt("Enter a number"));
+var a = 1;
+while (a <= num) {
+    if (a % 2 == 0) {
+        console.log(a);
+    }
+    a++;
+}
+*/
+
+// Print Odd Numbers
+/*
+var num = Number(prompt("Enter a number"));
+var a = 1;
+while (a <= num) {
+    if (a % 2 != 0) {
+        console.log(a);
+    }
+    a++;
+}
+*/
+
+// ask a user any number and print its table
+
+var num = Number(prompt("Enter a number"));
+var a = 1;
+while (a <= 10) {
+    console.log(num + ' X ' + a + ' = ' + num * a);
+    a++;
+}
